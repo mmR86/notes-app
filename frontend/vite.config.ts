0504d553cbb6d5cb5472c4ed74796ba43987e211
Marin Milotić <mGitM101@gmail.com>
2025-07-15 15:51:30 +0200

    Decresesed the Vite version from 7 to 5 to enable development preview

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 5173,
+    proxy: {
+      // optional if you're connecting to Laravel API
+      "/api": "http://localhost:8000",
+    },
+  },
+});

d4d13377f0d33d8cab7cdc0a4c716580b9735c6b
Marin Milotić <mGitM101@gmail.com>
2025-07-15 17:25:36 +0200

    Installed Axios

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,6 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="p-10 bg-gray-200 min-h-screen">
       <h1 className="text-4xl font-bold text-blue-600">Hello Tailwind!</h1>

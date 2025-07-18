895423cfd7b137b0a3a0c984c12908bae690ad8d
Marin Milotić <mGitM101@gmail.com>
2025-07-18 13:55:24 +0200

    Created and implemented the Layout, deleted useAuth hook

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,15 +4,18 @@ import Notes from "./pages/Notes";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
+import MainLayout from "./layouts/MainLayout";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/notes" element={<Notes />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/notes" element={<Notes />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Route>
       </Routes>
     </Router>
   );
